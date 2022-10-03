e969d3983096d43e1f5665def6e2123b30640e1d
Nicolas Kruchten <nicolas@plot.ly>
2022-10-03 12:45:43 -0400

    more idiomatic/terser bubble charts

--- studies/movies/ggplot2/bubble.R
+++ studies/movies/ggplot2/bubble.R
@@ -4,5 +4,5 @@ df <- read_csv("data/movies.csv")
 ggplot(df) +
   geom_point(aes(
     x = `Production Budget`, y = `Worldwide Gross`,
-    color = `MPAA Rating`, size = `Rotten Tomatoes Rating`
+    color = `MPAA Rating`, size = `IMDB Rating`
   ))
--- studies/movies/ggplot2/bubble_agg.R
+++ studies/movies/ggplot2/bubble_agg.R
@@ -1,12 +1,8 @@
 library(tidyverse)
 
-df <- read_csv("/Users/nicolas/ets/notascope/data/movies.csv") |>
+df <- read_csv("data/movies.csv") |>
   group_by(`MPAA Rating`) |>
-  summarize(
-    `Production Budget` = mean(`Production Budget`, na.rm = TRUE),
-    `Worldwide Gross` = mean(`Worldwide Gross`, na.rm = TRUE),
-    `IMDB Rating` = mean(`IMDB Rating`, na.rm = TRUE)
-  )
+  summarize_all(~ mean(.x, na.rm = TRUE))
 
 ggplot(df) +
   geom_point(aes(
