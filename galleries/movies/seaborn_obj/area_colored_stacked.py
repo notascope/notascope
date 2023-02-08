fe9f7d95a2a8ac2b0a122edd4112de180fd4df8c
Nicolas Kruchten <nicolas@kruchten.com>
2023-02-07 19:57:36 -0500

    seaborn obj stacking

--- galleries/movies/seaborn_obj/area_colored_stacked.py
+++ galleries/movies/seaborn_obj/area_colored_stacked.py
@@ -3,11 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("data/movies.csv")
 df["Release Date"] = pd.to_datetime(df["Release Date"]).dt.year
-df2 = df.groupby(["Release Date", "MPAA Rating"])["Worldwide Gross"].sum().reset_index()
-df2["Worldwide Gross"] /= df2.groupby("Release Date")["Worldwide Gross"].transform(
-    "sum"
-)
 
-p = so.Plot(df2, x="Release Date", y="Worldwide Gross", color="MPAA Rating").add(
-    so.Area(), so.Stack()
+p = so.Plot(df, x="Release Date", y="Worldwide Gross", color="MPAA Rating").add(
+    so.Area(), so.Agg("sum"), so.Norm("sum", by=["x"]), so.Stack()
 )
--- galleries/movies/seaborn_obj/bar_count_100.py
+++ galleries/movies/seaborn_obj/bar_count_100.py
@@ -2,8 +2,7 @@ import seaborn.objects as so
 import pandas as pd
 
 df = pd.read_csv("data/movies.csv")
-df2 = df.groupby(["Major Genre", "MPAA Rating"]).size().reset_index()
-df2[0] /= df2.groupby("Major Genre")[0].transform("sum")
-p = so.Plot(df2, x="Major Genre", y=0, color="MPAA Rating").add(
-    so.Bar(), so.Agg("sum"), so.Stack()
+
+p = so.Plot(df, x="Major Genre", y=0, color="MPAA Rating").add(
+    so.Bar(), so.Agg("sum"), so.Norm("sum", by=["x"]), so.Stack()
 )
