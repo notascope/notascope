170ed34c8bfaf6c417dc83cb9f7fa7f779144fc3
Nicolas Kruchten <nicolas@kruchten.com>
2023-02-16 22:13:28 -0500

    standardizing galleries a bit

--- galleries/movies/plotly_express/dotplot_avg.py
+++ galleries/movies/plotly_express/dotplot_avg.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data/movies.csv").groupby("Major Genre").median().reset_index()
+df = (
+    pd.read_csv("data/movies.csv")
+    .groupby("Major Genre")["Production Budget"]
+    .mean()
+    .reset_index()
+)
 fig = px.scatter(df, x="Production Budget", y="Major Genre")
 fig
--- galleries/movies/plotly_go/slope.py
+++ galleries/movies/plotly_go/slope.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.graph_objects as go
 
 df = pd.read_csv("data/movies.csv")
-df["Release Date"] = pd.to_datetime(df["Release Date"]).dt.year
 df2 = df.pivot_table(
     index="Major Genre",
     columns="MPAA Rating",
