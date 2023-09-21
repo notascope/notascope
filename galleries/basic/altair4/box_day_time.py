ce6c08eae755884593e8e86e8229398a0b058bd7
Nicolas Kruchten <nicolas@kruchten.com>
2023-09-20 22:10:36 -0400

    remove altair4

--- galleries/basic/altair4/box_day_time.py
+++
@@ -1,9 +0,0 @@
-import altair as alt
-from plotly.data import tips
-
-chart = (
-    alt.Chart(tips())
-    .mark_boxplot()
-    .encode(x="total_bill", color="time", y="time", facet="day")
-)
-chart
--- notations/savers/altair4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-{cat $1 ; echo "from altair_saver import save; save(chart, '$2.svg', method='node')"} | python
